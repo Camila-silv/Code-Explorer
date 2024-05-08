10d6b8b18d47c9845c8d48dd45ba6421f6352a17
Camila <cf.silv466@gmail.com>
2024-05-08 15:30:24 -0300

    Add files via upload

---
+++ desafio#01/portfolio/src/components/Footer/index.jsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <footer className="main-footer">
+      <span className="main-footer__parag">
+        Desenvolvido por{" "}
+        <a
+          href="https://www.linkedin.com/in/camila-silv/"
+          className="footer-link"
+          target="_blanck"
+        >
+          Camila Silva
+        </a>
+        .
+      </span>
+    </footer>
+  );
+}
