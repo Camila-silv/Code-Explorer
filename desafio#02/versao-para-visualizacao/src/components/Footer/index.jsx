b1ed638c0d13fb998d21000f786c287303f0cce4
Camila <cf.silv466@gmail.com>
2024-05-08 15:08:44 -0300

    Desafio #2 realizado.

---
+++ desafio#02/versao-para-visualizacao/src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <footer className="main-footer">
+      <span className="main-footer__lbl">
+        Desenvolvido por Camila -{" "}
+        <a
+          href="https://nicepage.com/pt/website-templates/preview/portfolio-262120?device=desktop"
+          target="_blank"
+          className="main-footer__link"
+        >
+          Modelo de layout
+        </a>
+      </span>
+    </footer>
+  );
+}
